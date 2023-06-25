837487022d513237b1b4aa0794fd364724301a11
Bruno Fontes <brunobfonttes@gmail.com>
2023-06-24 21:30:29 -0300

    refactor: replaces the create hash function from test file with the original createHash

--- dpk.test.js
+++ dpk.test.js
@@ -1,36 +1,37 @@
-const crypto = require("crypto");
-const { deterministicPartitionKey, MAX_PARTITION_KEY_LENGTH } = require("./dpk");
-
-const getsHashOf = (data)=> crypto.createHash("sha3-512").update(data).digest("hex");
+const {
+  deterministicPartitionKey,
+  createHash,
+  MAX_PARTITION_KEY_LENGTH,
+} = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
-  it("Returns the string event.partitionKey", ()=>{
-    const trivialKey = deterministicPartitionKey({partitionKey:"123"})
+  it("Returns the string event.partitionKey", () => {
+    const trivialKey = deterministicPartitionKey({ partitionKey: "123" })
     expect(trivialKey).toBe("123")
   })
-  it("Returns stringified event.partitionKey", ()=>{
-    const obj = { value: 123}
-    const trivialKey = deterministicPartitionKey({partitionKey:obj})
+  it("Returns stringified event.partitionKey", () => {
+    const obj = { value: 123 }
+    const trivialKey = deterministicPartitionKey({ partitionKey: obj })
     expect(trivialKey).toBe(JSON.stringify(obj))
   })
-  it("Returns hashed stringified event", ()=>{
+  it("Returns hashed stringified event", () => {
     const event = { value: 123 }
     const trivialKey = deterministicPartitionKey(event)
-    expect(trivialKey).toBe(getsHashOf(JSON.stringify(event)))
+    expect(trivialKey).toBe(createHash(JSON.stringify(event)))
   })
-  it("Returns hashed string event", ()=>{
+  it("Returns hashed string event", () => {
     const event = "123469987745"
     const trivialKey = deterministicPartitionKey(event)
-    expect(trivialKey).toBe(getsHashOf(JSON.stringify(event)))
+    expect(trivialKey).toBe(createHash(JSON.stringify(event)))
   })
-  it("Returns hashed event.partitionkey", ()=>{
-    const event = { partitionKey: 'x'.repeat(MAX_PARTITION_KEY_LENGTH*10)  }
+  it("Returns hashed event.partitionkey", () => {
+    const event = { partitionKey: 'x'.repeat(MAX_PARTITION_KEY_LENGTH * 10) }
     const trivialKey = deterministicPartitionKey(event)
-    expect(trivialKey).toBe(getsHashOf(event.partitionKey))
+    expect(trivialKey).toBe(createHash(event.partitionKey))
   })
-  
+
 });
